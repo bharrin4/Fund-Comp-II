4e9a6916b5d5aed20988a2cf9bb701644e80917d
bharrin4 <bharrin4@nd.edu>
2015-08-29 15:19:13 -0400

    Program that creates a puzzle and solves it using Puzzle.h class

---
+++ solver.cpp
@@ -0,0 +1,17 @@
+//Brittany Harrington
+//Fundamentals of Computing II
+//March 5, 2015
+//Solver.cpp program that instantiates a puzzle of the Puzzle.h class and solves it.
+//
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <vector>
+#include "Puzzle.h"
+
+using namespace std;
+
+int main()	{
+	Puzzle puzzle;
+	puzzle.play();
+}
